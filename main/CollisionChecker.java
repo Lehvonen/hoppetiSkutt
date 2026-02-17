cda05baf18360a1fb2c2c2af0bc9dbfb726e5f6d
Ivar Lehvonen <lehvonen.ivar@gmail.com>
2026-02-17 15:04:25 +0200

    Add object collision checking in CollisionChecker

--- main/CollisionChecker.java
+++ main/CollisionChecker.java
@@ -1,14 +1,21 @@
 package main;
 
 import entity.Entity;
+import entity.Player;
+import object.SuperObject;
+
+import java.util.Objects;
 
 public class CollisionChecker {
 
     GamePanel gp;
 
+
     public CollisionChecker(GamePanel gp){
         this.gp = gp;
     }
+
+
 
     public void checkTile(Entity entity){
 
@@ -21,6 +28,8 @@ public class CollisionChecker {
         int entityRightCol = entityRightWorldX/gp.tileSize;
         int entityTopRow = entityTopWorldY/gp.tileSize;
         int entityBottomRow = entityBottomWorldY/gp.tileSize;
+
+
 
         int tileNum1, tileNum2;
 
@@ -60,5 +69,80 @@ public class CollisionChecker {
         }
 
     }
-}
+    public int checkObject(Entity entity, boolean player){
 
+        int index = 999;
+
+        for(int i = 0; i < gp.obj.length; i++){
+            if(gp.obj[i] != null) {
+                entity.solidArea.x = entity.worldX + entity.solidArea.x;
+                entity.solidArea.y = entity.worldY + entity.solidArea.y;
+
+                gp.obj[i].solidArea.x = gp.obj[i].worldX + gp.obj[i].solidArea.x;
+                gp.obj[i].solidArea.y = gp.obj[i].worldY + gp.obj[i].solidArea.y;
+
+                switch (entity.direction) {
+                    case "up":
+                        entity.solidArea.y -= entity.speed;
+                        if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
+                            if (gp.obj[i].collision) {
+                                entity.collisionOn = true;
+                            }
+                            if (player) {
+                                index = i;
+                            }
+                        }
+                        entity.solidArea.y -= entity.speed;
+                            break;
+                            case "down":
+                                if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
+                                    if (gp.obj[i].collision) {
+                                        entity.collisionOn = true;
+                                    }
+                                    if (player) {
+                                        index = i;
+                                    }
+
+                                }
+                                entity.solidArea.y += entity.speed;
+                                break;
+                            case "left":
+                                if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
+                                    if (gp.obj[i].collision) {
+                                        entity.collisionOn = true;
+                                    }
+                                    if (player) {
+                                        index = i;
+                                    }
+                                }
+                                entity.solidArea.x += entity.speed;
+                                break;
+                            case "right":
+                                if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
+                                    if (gp.obj[i].collision) {
+                                        entity.collisionOn = true;
+                                    }
+                                    if (player) {
+                                        index = i;
+                                    }
+                                }
+                                entity.solidArea.x -= entity.speed;
+                                break;
+                        }
+
+                        entity.solidArea.x = entity.solidAreaDefaultX;
+                        entity.solidArea.y = entity.solidAreaDefaultY;
+                        gp.obj[i].solidArea.x = gp.obj[i].solidAreaDefaultX;
+                        gp.obj[i].solidArea.y = gp.obj[i].solidAreaDefaultY;
+
+
+                }
+
+
+
+
+        }
+        return index;
+    }
+
+}
