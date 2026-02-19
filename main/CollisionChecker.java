57f9bfd682f0cbc8b4a2794a354b3327d165fe14
Ivar Lehvonen <lehvonen.ivar@gmail.com>
2026-02-19 15:55:00 +0200

    Refactor collision detection logic in CollisionChecker

--- main/CollisionChecker.java
+++ main/CollisionChecker.java
@@ -39,6 +39,7 @@ public class CollisionChecker {
                 tileNum1 = gp.tileM.mapTileNum[entityLeftCol][entityTopRow];
                 tileNum2 = gp.tileM.mapTileNum[entityRightCol][entityTopRow];
                 if(gp.tileM.tile[tileNum1].collision || gp.tileM.tile[tileNum2].collision ){
+                    gp.playSE(3);
                     entity.collisionOn = true;
                 }
                 break;
@@ -47,6 +48,7 @@ public class CollisionChecker {
                 tileNum1 = gp.tileM.mapTileNum[entityLeftCol][entityBottomRow];
                 tileNum2 = gp.tileM.mapTileNum[entityRightCol][entityBottomRow];
                 if(gp.tileM.tile[tileNum1].collision || gp.tileM.tile[tileNum2].collision ){
+                    gp.playSE(3);
                     entity.collisionOn = true;
                 }
                 break;
@@ -55,6 +57,7 @@ public class CollisionChecker {
                 tileNum1 = gp.tileM.mapTileNum[entityLeftCol][entityTopRow];
                 tileNum2 = gp.tileM.mapTileNum[entityLeftCol][entityBottomRow];
                 if(gp.tileM.tile[tileNum1].collision || gp.tileM.tile[tileNum2].collision ){
+                    gp.playSE(3);
                     entity.collisionOn = true;
                 }
                 break;
@@ -63,10 +66,12 @@ public class CollisionChecker {
                 tileNum1 = gp.tileM.mapTileNum[entityRightCol][entityTopRow];
                 tileNum2 = gp.tileM.mapTileNum[entityRightCol][entityBottomRow];
                 if(gp.tileM.tile[tileNum1].collision || gp.tileM.tile[tileNum2].collision ){
+                    gp.playSE(3);
                     entity.collisionOn = true;
                 }
                 break;
         }
+
 
     }
     public int checkObject(Entity entity, boolean player){
@@ -93,50 +98,50 @@ public class CollisionChecker {
                             }
                         }
                         entity.solidArea.y -= entity.speed;
-                            break;
-                            case "down":
-                                if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
-                                    if (gp.obj[i].collision) {
-                                        entity.collisionOn = true;
-                                    }
-                                    if (player) {
-                                        index = i;
-                                    }
+                        break;
+                    case "down":
+                        if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
+                            if (gp.obj[i].collision) {
+                                entity.collisionOn = true;
+                            }
+                            if (player) {
+                                index = i;
+                            }
 
-                                }
-                                entity.solidArea.y += entity.speed;
-                                break;
-                            case "left":
-                                if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
-                                    if (gp.obj[i].collision) {
-                                        entity.collisionOn = true;
-                                    }
-                                    if (player) {
-                                        index = i;
-                                    }
-                                }
-                                entity.solidArea.x += entity.speed;
-                                break;
-                            case "right":
-                                if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
-                                    if (gp.obj[i].collision) {
-                                        entity.collisionOn = true;
-                                    }
-                                    if (player) {
-                                        index = i;
-                                    }
-                                }
-                                entity.solidArea.x -= entity.speed;
-                                break;
                         }
-
-                        entity.solidArea.x = entity.solidAreaDefaultX;
-                        entity.solidArea.y = entity.solidAreaDefaultY;
-                        gp.obj[i].solidArea.x = gp.obj[i].solidAreaDefaultX;
-                        gp.obj[i].solidArea.y = gp.obj[i].solidAreaDefaultY;
-
-
+                        entity.solidArea.y += entity.speed;
+                        break;
+                    case "left":
+                        if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
+                            if (gp.obj[i].collision) {
+                                entity.collisionOn = true;
+                            }
+                            if (player) {
+                                index = i;
+                            }
+                        }
+                        entity.solidArea.x += entity.speed;
+                        break;
+                    case "right":
+                        if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
+                            if (gp.obj[i].collision) {
+                                entity.collisionOn = true;
+                            }
+                            if (player) {
+                                index = i;
+                            }
+                        }
+                        entity.solidArea.x -= entity.speed;
+                        break;
                 }
+
+                entity.solidArea.x = entity.solidAreaDefaultX;
+                entity.solidArea.y = entity.solidAreaDefaultY;
+                gp.obj[i].solidArea.x = gp.obj[i].solidAreaDefaultX;
+                gp.obj[i].solidArea.y = gp.obj[i].solidAreaDefaultY;
+
+
+            }
 
 
 
