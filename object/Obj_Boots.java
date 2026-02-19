36033a1aeb34fd1b1a7b806c8f8e1de608efad56
Ivar Lehvonen <lehvonen.ivar@gmail.com>
2026-02-19 15:58:19 +0200

    Create Obj_Boots class with image handling
    Add Obj_Boots class to represent boots object with image loading.

---
+++ object/Obj_Boots.java
@@ -0,0 +1,18 @@
+package object;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
+public class Obj_Boots  extends SuperObject{
+
+    public Obj_Boots(){
+
+        name = "Boots";
+        try{
+            image = ImageIO.read(getClass().getResourceAsStream("/res/objects/Boots.png"));
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
