e229c45117490ce43aa683e0df68ba6678a412ae
Ivar Lehvonen <lehvonen.ivar@gmail.com>
2026-02-19 15:59:12 +0200

    Add Obj_Sword class with image loading

--- object/Obj_Sword.java
+++ object/Obj_Sword.java
@@ -1,1 +1,23 @@
 
+package object;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Obj_Sword  extends SuperObject{
+
+    public Obj_Sword(){
+
+        name = "Door";
+        try{
+            image = ImageIO.read(getClass().getResourceAsStream("/res/objects/Sword.png"));
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        collision = false;
+    }
+
+
+}
