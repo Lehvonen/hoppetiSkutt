69a6fa7432cf8f9ec7311f1b6d17a5c2e1df1e20
Ivar Lehvonen <lehvonen.ivar@gmail.com>
2026-02-19 15:56:09 +0200

    Refactor Sound class for improved audio management
    Refactored Sound class to initialize sound URLs and modify file handling.

--- main/Sound.java
+++ main/Sound.java
@@ -1,40 +1,48 @@
 package main;
 
-import javax.sound.sampled.*;
-import java.io.IOException;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import java.net.URL;
 
 public class Sound {
 
-    public Clip clip;
+    Clip clip;
+    URL[] soundURL = new URL[30];
 
-    public void setFile(String soundPath) {
-        try {
-            URL url = this.getClass().getResource(soundPath);
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(url);
+    public Sound(){
+        soundURL[0] = getClass().getResource("/res/sound/Background.wav");
+        soundURL[1] = getClass().getResource("/res/sound/Coin.wav");
+        soundURL[2] = getClass().getResource("/res/sound/LappaBiisi.wav");
+        soundURL[3] = getClass().getResource("/res/sound/Collision.wav");
+        soundURL[4] = getClass().getResource("/res/sound/Walking.wav");
+        soundURL[5] = getClass().getResource("/res/sound/DoorTp.wav");
+
+    }
+
+
+    public void setFile(int i){
+        try{
+            AudioInputStream ais = AudioSystem.getAudioInputStream(soundURL[i]);
             clip = AudioSystem.getClip();
-            clip.open(audioStream);
-        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
-            e.printStackTrace();
-        }
-    }
+            clip.open(ais);
+            FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            gainControl.setValue(-30.0f);
+        }catch (Exception e){
 
-    public void play() {
-        if (clip != null) {
-            clip.setFramePosition(0); // Rewind to the beginning
-            clip.start();
         }
-    }
 
-    public void loop() {
-        if (clip != null) {
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
-        }
-    }
 
-    public void stop() {
-        if (clip != null) {
-            clip.stop();
-        }
+    }
+    public void play(){
+        clip.start();
+    }
+    public void loop(){
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+    public void stop(){
+        clip.stop();
     }
 }
